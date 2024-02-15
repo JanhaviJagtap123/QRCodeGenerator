216f6420e1e358bff23aabca598b79d2af99e7a5
JanhaviJagtap123 <83360329+JanhaviJagtap123@users.noreply.github.com>
2024-02-15 14:23:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,33 +8,3 @@ function genQR(){
     const container = document.getElementById("qrCodeContainer");
     container.appendChild(qrCodeImg);
 }
-
-
-
-
-
-// {      
-//        var gapi = "https://chart.googleapis.com/chart?chs=";
-//        var myImg = document.getElementById("img");
-//        var myText = document.getElementById("qrtext").value;
-//        var mySize = document.getElementById("size").value;
-
-//        if(myText !== "" && mySize == 100){
-//            myImg.src = gapi+"100x100"+"&chl="+myText;
-//        }
-//        else if(myText !== "" && mySize == 150){
-//            myImg.src = gapi+"150x150"+"&chl="+myText;
-//        }
-//        else if(myText !== "" && mySize == 200){
-//            myImg.src = gapi+"200x200"+"&chl="+myText;
-//        }
-//        else if(myText !== "" && mySize == 250){
-//            myImg.src = gapi+"250x250"+"&chl="+myText;
-//        }
-//        else if(myText !== "" && mySize == 300){
-//            myImg.src = gapi+"300x300"+"&chl="+myText;
-//        }
-//        else{
-//            alert("Please enter text");
-//        }
-// }
